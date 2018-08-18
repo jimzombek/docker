a7c8a95392f83841b7abc20a551b7df3aad9814a
Jim Zombek <jimzombek@gmail.com>
2018-08-18 07:33:09 -0400

    initial push

---
+++ src/main/java/com/jmzombek/dockerized/Application.java
@@ -0,0 +1,12 @@
+package com.jmzombek.dockerized;
+
+import java.lang.String;
+import java.lang.Exception;
+
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+		System.out.println("Hello Dockerized JimZ");
+	}
+
+}
